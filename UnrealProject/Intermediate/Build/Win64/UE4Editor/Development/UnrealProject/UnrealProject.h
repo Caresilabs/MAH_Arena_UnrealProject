6b503269de2af91103ea76563e8066dad354f568
Bernhard <benny_boy_sos@hotmail.com>
2015-11-12 11:12:49 +0100

    Changed obj to objx on preliminary car model file

--- UnrealProject/Intermediate/Build/Win64/UE4Editor/Development/UnrealProject/UnrealProject.h
+++ UnrealProject/Intermediate/Build/Win64/UE4Editor/Development/UnrealProject/UnrealProject.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_UNREALPROJECT_H__
 #define __AUTO_UNREALPROJECT_H__
-//Last Write: 11/5/2015 12:04:31 PM +00:00
-#include "C:\Users\Simon\Documents\GitHub\MAH_Arena_UnrealProject\UnrealProject\Source\UnrealProject\UnrealProject.h"
+//Last Write: 2015-10-29 14:04:18 +00:00
+#include "C:\Users\Bernhard\Documents\BrainTrustGithub\UnrealProject\Source\UnrealProject\UnrealProject.h"
 #endif//__AUTO_UNREALPROJECT_H__
