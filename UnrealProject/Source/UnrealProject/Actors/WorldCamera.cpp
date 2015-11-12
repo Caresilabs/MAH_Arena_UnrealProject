64b7f286b4536dbb91d3b17eae06c4538a8ab99e
Simon Bothén <caresilabs@gmail.com>
2015-11-12 11:40:45 +0100

    AwesomeCamera

--- UnrealProject/Source/UnrealProject/Actors/WorldCamera.cpp
+++ UnrealProject/Source/UnrealProject/Actors/WorldCamera.cpp
@@ -5,25 +5,32 @@
 
 
 void AWorldCamera::Tick(float delta) {
-	float maxDistance = 0;
+	float maxDistance = 1;
 	FVector average(0,0,0);
-	
 	
 	for (AActor* act : Actors)
 	{
 		average = average + act->GetActorLocation();
-		if (maxDistance < (act->GetActorLocation() - GetActorLocation()).Size()) {
-			maxDistance = (act->GetActorLocation() - GetActorLocation()).Size();
+		
+		for (AActor* act2 : Actors)
+		{
+			if (maxDistance < (act->GetActorLocation() - act2->GetActorLocation()).Size()) {
+				maxDistance = (act->GetActorLocation() - act2->GetActorLocation()).Size();
+			}
 		}
+
+		//if (maxDistance < (act->GetActorLocation() - GetActorLocation()).Size()) {
+		//	maxDistance = (act->GetActorLocation() - GetActorLocation()).Size();
+		//}
 	}
 	average =  average / (float)Actors.Num();
 
 	//average.Z = 0;
 
 	FRotator Rot = FRotationMatrix::MakeFromX(average - GetActorLocation()).Rotator(); //Actors[0]->GetActorLocation()
-	SetActorRelativeRotation(Rot);
+	//SetActorRelativeRotation(Rot);
 	
-	SetActorLocation(average + -GetActorForwardVector() * maxDistance);
+	SetActorLocation(average + (-GetActorForwardVector() * maxDistance*1.7f));
 
 }
 
