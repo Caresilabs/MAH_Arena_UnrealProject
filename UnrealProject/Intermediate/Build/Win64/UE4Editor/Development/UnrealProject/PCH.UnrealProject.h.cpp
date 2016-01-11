329bfc0a773184be0b96508f301c0cc9f3e47777
Simon Bothén <caresilabs@gmail.com>
2016-01-11 09:58:22 +0100

    Bug fixes, angular damping.

--- UnrealProject/Intermediate/Build/Win64/UE4Editor/Development/UnrealProject/PCH.UnrealProject.h.cpp
+++ UnrealProject/Intermediate/Build/Win64/UE4Editor/Development/UnrealProject/PCH.UnrealProject.h.cpp
@@ -1,1 +1,1 @@
-#include "D:\Users\Tim\Documents\MAH_Arena_UnrealProject\UnrealProject\Source\UnrealProject\UnrealProject.h"
+#include "C:\Users\Simon\Documents\GitHub\MAH_Arena_UnrealProject\UnrealProject\Source\UnrealProject\UnrealProject.h"
--- UnrealProject/Intermediate/Build/Win64/UE4Editor/Development/UnrealProject/UnrealProject.h
+++ UnrealProject/Intermediate/Build/Win64/UE4Editor/Development/UnrealProject/UnrealProject.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_UNREALPROJECT_H__
 #define __AUTO_UNREALPROJECT_H__
-//Last Write: 2015-11-27 09:50:45 +00:00
-#include "D:\Users\Tim\Documents\MAH_Arena_UnrealProject\UnrealProject\Source\UnrealProject\UnrealProject.h"
+//Last Write: 11/12/2015 11:22:39 AM +00:00
+#include "C:\Users\Simon\Documents\GitHub\MAH_Arena_UnrealProject\UnrealProject\Source\UnrealProject\UnrealProject.h"
 #endif//__AUTO_UNREALPROJECT_H__
